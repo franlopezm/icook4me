d26f720298f19f3a49badbd85820edd863bff8f9
FransLopez <frans.lopez.m@gmail.com>
2017-03-20 13:03:30 +0100

    infinite scroll added HomeCtrl

--- client/js/ctrl/HomeCtrl.js
+++ client/js/ctrl/HomeCtrl.js
@@ -7,21 +7,20 @@
   function HomeCtrl ($rootScope, ApiRecipesFact) {
     $rootScope.section = 'home'
     let vm = this
-    ApiRecipesFact.getAllPopAutor()
-      .then(data => vm.recipes = data)
+    let showItems = 18
+    vm.aRecipes = []
 
-    vm.showMoreItems = function () {
-      vm.showNoResult = true
-      let page = pagesShown + 1
-      ProxyRecipesFact.searchFood2fork(query, page)
-        .then(recipes => {
-          vm.aRecipes.push(...recipes)
-        })
-      ProxyRecipesFact.searchEdamam(query, page)
-        .then(recipes => {
-          vm.aRecipes.push(...recipes)
-        })
-      pagesShown = page
+    ApiRecipesFact.getAllPopAutor()
+      .then(data => {
+        vm.recipes = data
+        vm.showResult = true
+      })
+
+    vm.showMoreItems = function (elem) {
+      vm.aRecipes.push(...elem.splice(0, showItems))
+      if (elem.length === 0) {
+        vm.busy = true
+      }
     }
   }
 })()
--- server/routes/api/recipes/handlers/getAllAutorPop.js
+++ server/routes/api/recipes/handlers/getAllAutorPop.js
@@ -3,6 +3,7 @@ const Recipe = require(__base + 'models/Recipe')
 module.exports = (req, res) => {
   Recipe
     .find()
+    .sort({createdAt: -1})
     .populate('autor', 'name image url')
     .then(recipes => res.json(recipes))
     .catch(err => res.status(500).json(err))
